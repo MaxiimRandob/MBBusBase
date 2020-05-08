1093331688a15490dad4556c4656a79fb0194528
MaxiimRandob <madrandob@gmail.com>
2020-05-08 18:08:35 +0300

    added basic POJO's

---
+++ src/main/java/persistance/entity/Bus.java
@@ -0,0 +1,29 @@
+package persistance.entity;
+
+public class Bus {
+    private int busId;
+    private String name;
+    private String status;
+    private String model;
+    private Route assignedRoute;
+
+    public int getBusId() {
+        return busId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Route getAssignedRoute() {
+        return assignedRoute;
+    }
+}
---
+++ src/main/java/persistance/entity/Driver.java
@@ -0,0 +1,8 @@
+package persistance.entity;
+
+public class Driver extends User{
+
+    public Driver(int discriminator) {
+        super(discriminator);
+    }
+}
